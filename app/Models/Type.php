03c38f32aa46cead79c660d10f45be9ef783ad0d
Antonio D'Angelo <reapercsp@gmail.com>
2023-08-01 19:55:58 +0200

    fix file merge simone, error in sqlstate slug

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -14,28 +14,4 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Type extends Model
 {
     use HasFactory;
-
-    public function projects_alessio() {
-        return $this->hasMany(ProjectAlessio::class);
-    }
-
-    public function projects_andrea() {
-        return $this->hasMany(ProjectAndrea::class);
-    }
-
-    public function projects_antonio() {
-        return $this->hasMany(ProjectAntonio::class);
-    }
-
-    public function projects_davide() {
-        return $this->hasMany(ProjectDavide::class);
-    }
-
-    public function projects_paolo() {
-        return $this->hasMany(ProjectPaolo::class);
-    }
-
-    public function projects_simone() {
-        return $this->hasMany(ProjectSimone::class);
-    }
 }
