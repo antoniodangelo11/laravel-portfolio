5341638112129a5d469782d39fee26fe24374c8f
Antonio D'Angelo <reapercsp@gmail.com>
2023-08-02 19:06:06 +0200

    add tech in api controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $project = Project::with('type', 'user')->paginate(6);
+        $project = Project::with('type', 'user', 'technologies')->paginate(6);
 
         return response()->json([
             'success' => true,
