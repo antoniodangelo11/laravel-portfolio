501ba83a2c5fd255e0bfc8a3e3cff74563f4ab4d
SimoneRicco <smnrcc0@gmail.com>
2023-08-10 14:14:43 +0200

    add column sorting

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,27 +9,42 @@ use App\Models\Technology;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Kyslik\ColumnSortable\Sortable;
 
 class Project extends Model
 {
     use HasFactory;
     use SoftDeletes;
     use Slugger;
+    use Sortable;
 
     protected $table = 'projects';
+
+    public $sortable = [
+        'id',
+        'title',
+        'creation_date',
+        'last_update',
+        'collaborators',
+        'type_id',
+        'user_id',
+    ];
 
     public function getRouteKey()
     {
         return $this->slug;
     }
 
-    public function type() {
+    public function type()
+    {
         return $this->belongsTo(Type::class);
     }
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-    public function technologies() {
+    public function technologies()
+    {
         return $this->belongsToMany(Technology::class);
     }
 }
