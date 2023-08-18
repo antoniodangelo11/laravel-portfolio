283426fe4e4082f03626601608826aad880f6529
DavideFarci <yank92.df@gmail.com>
2023-08-18 17:57:31 +0200

    add type controller

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Type;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TypeController extends Controller
+{
+    public function index() {
+        $types = Type::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $types,
+        ]);
+    }
+}
